0c01f7c7d51dfc6001357beb094c99e118310826
Shoaib Siddiqui <112450948+hassanssh@users.noreply.github.com>
2024-07-23 14:57:37 -0400

    Update Offline wm_strings_en.h

--- wm_strings_en.h
+++ wm_strings_en.h
@@ -52,6 +52,7 @@ const char * const HTTP_PORTAL_MENU[] PROGMEM = {
 "<form action='/exit'    method='get'><button>Exit</button></form><br/>\n",  // MENU_EXIT
 "<form action='/erase'   method='get'><button class='D'>Erase</button></form><br/>\n", // MENU_ERASE
 "<form action='/update'  method='get'><button>Update</button></form><br/>\n",// MENU_UPDATE
+"<form action='/Offline'  method='get'><button>Offline</button></form><br/>\n",// MENU_UPDATE
 "<hr><br/>" // MENU_SEP
 };
 
